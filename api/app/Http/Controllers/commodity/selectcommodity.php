1dbd762a7c319288a1720d25c5b31927c3a01bb9
凛冬 <384693801@qq.com>
2020-04-02 21:13:23 +0000

    update login
    登录和注册框完善，遮罩是真的烦人啊。。

--- api/app/Http/Controllers/commodity/selectcommodity.php
+++ api/app/Http/Controllers/commodity/selectcommodity.php
@@ -18,12 +18,27 @@ class selectcommodity extends Controller
 {
     public function selectall($kind)
     {
-        if ($kind == 'all') {//查询所有
+        if ($kind == 'all') { //查询所有
             $com = commodity::all();
             return response()->json($com);
-        }
-        else{
-            return 'waiting';
+        } else {
+            switch ($kind) {
+                case "lu":
+                    //查询鲁菜
+                    $com = commodity::where("comkind","鲁菜")->get();
+                    return response()->json($com);
+                    break;
+                case "chuan":
+                    //查询川菜
+                    $com = commodity::where("comkind","川菜")->get();
+                    return response()->json($com);
+                    break;
+                case "yue":
+                    //查询粤菜
+                    $com = commodity::where("comkind","粤菜")->get();
+                    return response()->json($com);
+                    break;
+            }
         }
     }
     // 不是命令创建的类要更新autoload！！！！控制台指令：compuser dump-autoload！！！
