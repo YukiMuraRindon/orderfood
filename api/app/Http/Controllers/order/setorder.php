1bac8a89b932988ae0ef9b0818c8758c45917b7a
凛冬 <384693801@qq.com>
2020-04-18 16:01:42 +0000

    后端完善了下单部分
    部分细节优化

--- api/app/Http/Controllers/order/setorder.php
+++ api/app/Http/Controllers/order/setorder.php
@@ -9,61 +9,84 @@ use Illuminate\Support\Facades\DB;
 
 class setorder extends Controller
 {
-    public function setorder(Request $request){
+    public function setorder(Request $request)
+    {
         $nickname = $request->input('nickname');
         $ordprice = $request->input('ordprice');
         $ordststus = $request->input('ordstatus');
         $sentstatus = $request->input('sentstatus');
-        
+
         $user = DB::table('user')
-        ->select('userid')
-        ->where([
-            ['nickname', '=', $nickname]
-        ])
-        ->first();
+            ->select('userid')
+            ->where([
+                ['nickname', '=', $nickname]
+            ])
+            ->first();
         $userid = $user->userid;
-        
+
         DB::table('order')->insert(
             [
                 'userid' => $userid,
                 'ordprice' => $ordprice,
                 'ordstatus' => $ordststus,
                 'sentstatus' => $sentstatus
-            ]);
+            ]
+        );
 
         $order = DB::table('order')
-        ->select('orderid')
-        ->where([
-            ['userid', '=', $userid]
-        ])
-        ->orderBy('orderid', 'desc')
-        ->first();
+            ->select('orderid')
+            ->where([
+                ['userid', '=', $userid]
+            ])
+            ->orderBy('orderid', 'desc')
+            ->first();
 
         return [
             'error_code' => '0',
             'orderid' => $order->orderid
         ];
     }
-    public function setorderdetail(Request $request){
+    public function setorderdetail(Request $request)
+    {
         $orderid = $request->input('orderid');
         $comid = $request->input('comid');
         $comname = $request->input('comname');
         $comkind = $request->input('comkind');
         $comnum = $request->input('comnum');
 
-        DB::table('orderdetail')->insert(
-            [
-                'orderid' => $orderid,
-                'comid' => $comid,
-                'comname' => $comname,
-                'comkind' => $comkind,
-                'comnum' => $comnum
-            ]);
-        
+        $stock = DB::table('commodity')
+            ->select('comstock', 'comname')
+            ->where([
+                ['comid', '=', $comid]
+            ])
+            ->first();
+
+        if (($stock->comstock) < $comnum) {
+            return [
+                'error_code' => '1',
+                'message' => $stock->comname + "库存不足！"
+            ];
+        } else {
+            DB::table('orderdetail')->insert(
+                [
+                    'orderid' => $orderid,
+                    'comid' => $comid,
+                    'comname' => $comname,
+                    'comkind' => $comkind,
+                    'comnum' => $comnum
+                ]
+            );
+
+            DB::table('commodity')->update(
+                [
+                    'comstock' => $stock->comstock - $comnum
+                ]
+            );
+
             return [
                 'error_code' => '0',
                 'message' => "下单成功"
             ];
-
+        }
     }
 }
